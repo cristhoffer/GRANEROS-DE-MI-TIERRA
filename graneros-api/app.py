6de88052a901b3a8fdf566974eb785c43b24be42
Brandon Bermudez <brandonmorales555@gmail.com>
2020-11-28 05:45:39 -0600

    subiendo estructura de api route y models

--- graneros-api/app.py
+++ graneros-api/app.py
@@ -1,12 +1,16 @@
 from flask import Flask
+from api.route import auth_controller
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:joker@localhost:5432/graneros_db"
+db = SQLAlchemy(app)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+migrate = Migrate(app, db)
 
+app.register_blueprint(auth_controller)
 
 if __name__ == '__main__':
     app.run()
